2ce39b92e96186849bd0ec8bf34b189012e71a81
Miguel Henriques <github@jmh.com.br>
2019-07-15 14:21:36 -0300

    Campos obrigatórios

--- vendedores/models.py
+++ vendedores/models.py
@@ -6,12 +6,10 @@ class Vendedor(models.Model):
     id_vendedor = models.AutoField(primary_key=True, verbose_name="Código vendedor")
     nome = models.CharField(max_length=100, null=False, verbose_name="Nome")
     cpf = models.CharField(max_length=14, null=False, verbose_name="CPF")
-    endereco = models.TextField(null=True, blank=True, verbose_name="Endereço")
-    telefone = models.CharField(
-        max_length=14, null=True, blank=True, verbose_name="Telefone"
-    )
-    idade = models.PositiveSmallIntegerField(null=True, blank=True, verbose_name="Idade")
-    email = models.EmailField(null=True, blank=True, verbose_name="E-mail")
+    endereco = models.TextField(null=False, verbose_name="Endereço")
+    telefone = models.CharField(max_length=14, null=False, verbose_name="Telefone")
+    idade = models.PositiveSmallIntegerField(null=False, verbose_name="Idade")
+    email = models.EmailField(null=False, verbose_name="E-mail")
     plano = models.ForeignKey(
         PlanoComissoes, on_delete=models.CASCADE, verbose_name="Plano de Comissão"
     )
