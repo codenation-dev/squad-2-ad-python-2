8aa4fe173f7b5547e8fa84476304770a6b0e65b0
joao.heckmann <joao.heckmann@lincros.com>
2019-07-24 18:02:10 -0300

    Adicionando cálculo de comissão sobre venda mensal

--- vendas/serializers.py
+++ vendas/serializers.py
@@ -1,10 +1,23 @@
 from rest_framework import serializers
 from .models import Venda
-
-# Serializers define the API representation.
+from decimal import Decimal
 
 
 class VendasSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        vendas_mensais = validated_data["valor"]
+        plano_do_vendedor = validated_data["id_vendedor"].plano
+        comissao = Decimal(0.0)
+
+        if vendas_mensais:
+            if plano_do_vendedor.valor_minimo > vendas_mensais:
+                comissao = (plano_do_vendedor.percent_max / 100) * vendas_mensais
+            else:
+                comissao = (plano_do_vendedor.percent_min / 100) * vendas_mensais
+
+        validated_data["comissao"] = comissao
+        return Venda.objects.create(**validated_data)
+
     class Meta:
         model = Venda
         fields = ("id_venda", "id_vendedor", "mes", "valor")
--- vendas/tests.py
+++ vendas/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from planos.models import PlanoComissoes
+from vendedores.models import Vendedor
+from vendas.models import Venda
 
 # Create your tests here.
+class VendasModelTests(TestCase):
+
+    def test_deve_calcular_comissao_mensal(self):
+        """
+        Deve calcular a comissão em cima de uma venda mensal
+        """
+        Venda(id_vendedor=1)
