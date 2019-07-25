a79c87a0af397a020f86bb483743d0be263d987c
Miguel Henriques <github@jmh.com.br>
2019-07-25 15:19:28 -0300

    Notificação do vendedor
    Correção do cálculo
    
    Alteração do nomes
    
    Correção na lócica

--- vendas/views.py
+++ vendas/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.decorators import api_view
 from .models import Venda
 from .serializers import VendasSerializer
-# Create your views here.
+from rest_framework.response import Response
 
 
 class VendasViewSet(viewsets.ModelViewSet):
     queryset = Venda.objects.all()
     serializer_class = VendasSerializer
+
+
+@api_view(["POST"])
+def notificar_vededores(request):
+
+    vendedor = request.query_params["vendedor"]
+    valor = request.query_params["valor"]
+
+    vendas = Venda.objects.filter(id_vendedor=vendedor).order_by("-comissao")[:5]
+    total = 0
+    for cont, venda in enumerate(vendas):
+        total = total + (len(vendas) - cont) * venda.comissao
+    media = float(total / len(vendas)) * 0.9
+
+    if media < float(valor):
+        return Response({"notificar": True})
+    else:
+        return Response({"notificar": False})
+
