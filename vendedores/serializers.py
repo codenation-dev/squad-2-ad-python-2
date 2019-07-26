dc3bbbc4f4adcc7da3ea5d322fdce1e62019d5c6
Guilherme Lemke <guilherme.lemke@gmail.com>
2019-07-26 09:34:39 -0300

    Inclusão de comissoes/1

--- vendedores/serializers.py
+++ vendedores/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Vendedor
+from vendas.models import Venda
 
 # Serializers define the API representation.
 
@@ -8,3 +9,9 @@ class VendedoresSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vendedor
         fields = ("id_vendedor", "nome", "cpf", "endereco", "idade", "email", "plano")
+
+
+class ComissaoMensalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Venda
+        fields = ("id_vendedor", "mes", "comissao")
